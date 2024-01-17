5eb41d72f23cbdc4e044500d064d3b7b378c1141
ADMIN <ADMIN@PC327>
2024-01-17 10:07:49 +0700

    Add project files.

---
+++ DEO1CAU2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEO1CAU2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
